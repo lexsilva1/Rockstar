6f5503ddef911790c338b0efd0629a17052ef6c3
lexsilva1 <xanosilva@gmail.com>
2023-12-05 16:49:33 +0000

    teste de classes

--- Rockstar.java
+++ Rockstar.java
@@ -1,2 +1,5 @@
+import java.util.ArrayList;
+
 public class Rockstar {
+    private ArrayList<Utilizador> utilizadores;
 }
---
+++ Utilizador.java
@@ -0,0 +1,18 @@
+public abstract class Utilizador {
+    private String username;
+    private String password;
+    private String nome;
+    private String NIF;
+    private String email;
+    private boolean activo;
+
+    public Utilizador(String username, String password, String nome, String NIF, String email, boolean activo) {
+        this.username = username;
+        this.password = password;
+        this.nome = nome;
+        this.NIF = NIF;
+        this.email = email;
+        this.activo = activo;
+    }
+
+}
