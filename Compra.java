9a03dae39b5e1248972c062a599e282d603589e2
lexsilva1 <xanosilva@gmail.com>
2023-12-05 17:20:42 +0000

    update compra

--- Compra.java
+++ Compra.java
@@ -1,2 +1,12 @@
+import java.time.LocalDate;
+import java.util.ArrayList;
+
 public class Compra {
+    private LocalDate data;
+    private ArrayList<Musica> musicas;
+
+    public Compra() {
+        this.data=LocalDate.now();
+        this.musicas=new ArrayList<>();
+    }
 }
