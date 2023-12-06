79f401c3730eb6174c1cf7efe414ac2aeb8b563a
lexsilva1 <xanosilva@gmail.com>
2023-12-06 23:46:13 +0000

    construtores

--- Musica.java
+++ Musica.java
@@ -1,2 +1,33 @@
+import java.time.LocalDate;
+import java.util.ArrayList;
+
 public class Musica {
+    private String titulo;
+    private Musico autor;
+    private String genero;
+    private boolean activa;
+    private LocalDate dataLancamento;
+    private ArrayList<Preco> historicoPreco;
+    private ArrayList<Integer> classificacoes;
+    private ArrayList<String> votosUtilizadores;
+
+    public Musica(String titulo, String genero, LocalDate data, Musico autor,double valor) {
+        this.titulo = titulo;
+        this.genero = genero;
+        this.dataLancamento=data;
+        this.activa=true;
+        this.historicoPreco=new ArrayList<>();
+        Preco preco= new Preco(valor);
+        this.historicoPreco.add(preco);
+        this.classificacoes=new ArrayList<>();
+        this.votosUtilizadores=new ArrayList<>();
+        this.autor=autor;
+    }
+    public void alterarPreco(double preco){
+        Preco novopreco= new Preco(preco);
+        historicoPreco.add(novopreco);
+    }
+    public double getPreco(){
+        return historicoPreco.getLast().getPreco();
+    }
 }
---
+++ Preco.java
@@ -0,0 +1,15 @@
+import java.time.LocalDate;
+
+public class Preco {
+    private double preco;
+    private LocalDate data;
+
+    public Preco(double preco) {
+        this.preco = preco;
+        this.data=LocalDate.now();
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+}
