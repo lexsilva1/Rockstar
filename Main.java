1f85db1b257074e16062204776c4089e5689af79
petersundays <152998554+petersundays@users.noreply.github.com>
2023-12-14 17:06:18 +0000

    criei pastas gui e resources

--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
+import GUI.Janela;
+
 public class Main {
     public static void main(String[] args) {
-         Rockstar rockstar = new Rockstar();
+
+        Janela janela = new Janela();
     }
 }
