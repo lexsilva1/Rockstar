b7ec09bee33656b2e5e5d4823c8ba72e766ddd40
petersundays <petersundays@users.noreply.github.com>
2024-01-06 00:45:39 +0000

    avanço painel musico

--- GUI/PainelCarrinhoCompras.java
+++ GUI/PainelCarrinhoCompras.java
@@ -6,7 +6,9 @@ import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 import java.text.DecimalFormat;
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Locale;
 
 public class PainelCarrinhoCompras extends JPanel {
     private Cliente cliente;
@@ -91,7 +93,9 @@ public class PainelCarrinhoCompras extends JPanel {
 
         // Adicionar dados de exemplo à tabela
         for (Promo a : framePrincipal.getRockstar().getPromos()) {
-            modelotabelapromo.addRow(new Object[]{a.getNome(), a.getDesconto(), a.getDataInicio(), a.getDataFim(), a.getCupoes()});
+            if (a.getDataFim().isAfter(LocalDate.now()) && a.getCupoes() > 0) {
+                modelotabelapromo.addRow(new Object[]{a.getNome(), a.getDesconto(), a.getDataInicio(), a.getDataFim(), a.getCupoes()});
+            }
         }
 
 
