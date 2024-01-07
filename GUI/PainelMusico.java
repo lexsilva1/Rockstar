45f37cacd2d1fe49cc11255badc43c2421f09995
petersundays <petersundays@users.noreply.github.com>
2024-01-07 22:44:00 +0000

    avanço painel musico

--- GUI/PainelMusico.java
+++ GUI/PainelMusico.java
@@ -35,28 +35,28 @@ public class PainelMusico extends JPanel {
         setBackground(new Color(70, 90, 120));
         setLayout(null);
 
-        labelUsername.setBounds(20,5,200,25);
+        labelUsername.setBounds(20,5,180,25);
         labelUsername.setForeground(Color.WHITE);
 
-        lblPesquisar.setBounds(200  , 5, 100, 25);
+        lblPesquisar.setBounds(190  , 5, 80, 25);
         lblPesquisar.setForeground(Color.WHITE);
         lblPesquisar.setVisible(true);
         add(lblPesquisar);
 
-        txtPesquisar.setBounds(270, 5, 200, 25);
+        txtPesquisar.setBounds(250, 5, 200, 25);
         txtPesquisar.setVisible(true);
         add(txtPesquisar);
 
-        btnLupa.setBounds(480,5,20,20);
+        btnLupa.setBounds(450,7,20,20);
 
         JRadioButton chkPesquisaNome = new JRadioButton("Nome");
-        chkPesquisaNome.setBounds(270, 25, 100, 25);
+        chkPesquisaNome.setBounds(250, 27, 100, 25);
         chkPesquisaNome.setBackground(new Color(70, 90, 120));
         chkPesquisaNome.setForeground(Color.WHITE);
         add(chkPesquisaNome);
 
         JRadioButton chkPesquisaGenero = new JRadioButton("Género");
-        chkPesquisaGenero.setBounds(390, 25, 100, 25);
+        chkPesquisaGenero.setBounds(370, 27, 100, 25);
         chkPesquisaGenero.setBackground(new Color(70, 90, 120));
         chkPesquisaGenero.setForeground(Color.WHITE);
         add(chkPesquisaGenero);
@@ -65,17 +65,17 @@ public class PainelMusico extends JPanel {
         grupo.add(chkPesquisaNome);
         grupo.add(chkPesquisaGenero);
 
-        btnLogout.setBounds(725, 10, 75, 25);
+        btnLogout.setBounds(710, 10, 75, 25);
         btnVerAlbuns.setBounds(20,100,200,25);
         btnVerMusicas.setBounds(20,140,200,25);
-        btnCriarAlbum.setBounds(500,10,100,25);
-        btnAddMusica.setBounds(600,10,100,25);
+        btnCriarAlbum.setBounds(490,10,100,25);
+        btnAddMusica.setBounds(590,10,100,25);
         painelOpcoes.setBounds(275,100,450,500);
 
-        Border contorno = BorderFactory.createMatteBorder(3, 3, 0, 3, Color.white);
-        Border contorno1 = BorderFactory.createMatteBorder(3, 3, 0, 3, Color.white);
-        Border contorno2 = BorderFactory.createMatteBorder(0, 3, 0, 3, Color.white);
-        Border contorno3 = BorderFactory.createMatteBorder(0, 3, 3, 3, Color.white);
+        Border contorno = BorderFactory.createMatteBorder(5, 5, 0, 5, Color.white);
+        Border contorno1 = BorderFactory.createMatteBorder(3, 5, 0, 5, Color.white);
+        Border contorno2 = BorderFactory.createMatteBorder(0, 5, 0, 5, Color.white);
+        Border contorno3 = BorderFactory.createMatteBorder(0, 5, 5, 5, Color.white);
         String espaco = " ";
 
         JLabel lblCabecalho = new JLabel(" ESTATÍSTICAS");
